4a99fc48706e9bcc00140f24d30e4203cd187c6b
hard.tender.blade <92087058+DenisMatsenko@users.noreply.github.com>
2023-03-31 20:46:20 +0200

    Added Password generation

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
-	"strconv"
 	"strings"
+	// "strings"
+	// "strconv"
+	// "strings"
 )
 
 var reader = bufio.NewReader(os.Stdin)
@@ -17,19 +20,80 @@ func checkError(err error) {
 }
 
 func main() {
-	fmt.Print("Enter text: ")
+	fmt.Println("Hello!")
 
-	// ? Read string from console
-	text, err := reader.ReadString('\n')
-	checkError(err)
+	Test()
 
-	// ? Remove \n from string
-	text = strings.TrimRight(text, "\n")
+	CreatePassword()
 
-	// ? Convert string to int
-	num, err := strconv.Atoi(text)
-	checkError(err)
+	// // ? Read string from console
+	// var text int
+	// _, err := fmt.Scanf("%d", &text)
+	// checkError(err)
+
+	// fmt.Print(num)
 
 
-	fmt.Println(num + 10)
+	// fmt.Println(num + 10)
+}
+
+type PasswordParameters struct {
+	passwordSecure int
+	passwordLength int
+}
+
+type Password struct {
+	word string
+	description string
+}
+
+func GeneratePassword(pp PasswordParameters) string {
+	// * changable password chars
+	arr := []string{"abcdefghijklmnopqrstuvwxyz", "0123456789", "!@#$%&*_+-,.?~"}
+
+	// ? Set end point for random
+	endPoint := 0
+	switch pp.passwordSecure {
+		case 1:
+			endPoint = len(arr[0])
+		case 2:
+			endPoint = len(arr[0]) + len(arr[1])
+		case 3:
+			endPoint = len(arr[0]) + len(arr[1]) + len(arr[2])
+	}
+
+	// ? Randomly generate every char in password
+	passwordChars := strings.Join(arr, "")
+	var password string = ""
+	for i := 0; i < pp.passwordLength; i++ {
+		password = fmt.Sprintf("%s%c", password, passwordChars[rand.Intn(endPoint)])
+	}
+
+	return password
+}
+
+func CreatePassword() {
+	// ? Init password parameters
+	pp := PasswordParameters{passwordSecure: 3, passwordLength: 20}
+	const countOfPasswords int = 5
+
+	// ? Generate passwords
+	var passwords [countOfPasswords]string
+	for index := range passwords {
+		passwords[index] = GeneratePassword(pp)
+	}
+
+	var password Password
+	password.word = passwords[0]
+	password.description = "pw description"
+
+
+	SaveLocal(password)
+
+	fmt.Println(passwords)
+}
+
+
+func SaveLocal(password Password) {
+	
 }
